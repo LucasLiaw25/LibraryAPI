31ed018c1a98a16f0398a340604a086446f04205
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-08 17:33:56 -0300

    feat: Create findByEmail method

--- src/main/java/com/liaw/dev/Library/repository/UserRepository.java
+++ src/main/java/com/liaw/dev/Library/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.liaw.dev.Library.repository;
 import com.liaw.dev.Library.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
