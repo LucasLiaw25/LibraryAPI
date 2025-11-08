08e53df70adf877afbde35e11c0db0b8bb7261c1
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-08 17:33:30 -0300

    feat: Custom UserDetailService

--- src/main/java/com/liaw/dev/Library/service/CustomUserDetail.java
+++ src/main/java/com/liaw/dev/Library/service/CustomUserDetail.java
@@ -1,4 +1,21 @@
 package com.liaw.dev.Library.service;
 
-public class CustomUserDetail {
+import com.liaw.dev.Library.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetail implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(()-> new UsernameNotFoundException("Usuário não encontrado"));
+    }
 }
