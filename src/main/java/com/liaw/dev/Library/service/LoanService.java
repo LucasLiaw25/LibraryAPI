9eb397210b0d15bf2a86b1d6d428f15116bf79cd
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-08 15:23:25 -0300

    feat: Create the clesses of service

---
+++ src/main/java/com/liaw/dev/Library/service/LoanService.java
@@ -0,0 +1,9 @@
+package com.liaw.dev.Library.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoanService {
+}
