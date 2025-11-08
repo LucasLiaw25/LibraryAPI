f792a9f0ddda016c59e1d2bbdd9ca1dfb2bde552
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-08 16:18:40 -0300

    feat: Create endpoints for Book and User

--- src/main/java/com/liaw/dev/Library/controller/BookController.java
+++ src/main/java/com/liaw/dev/Library/controller/BookController.java
@@ -1,11 +1,50 @@
 package com.liaw.dev.Library.controller;
 
+import com.liaw.dev.Library.dto.BookDTO;
+import com.liaw.dev.Library.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/library/book")
 public class BookController {
+
+    private final BookService service;
+
+    @PostMapping
+    public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.createBook(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> listBooks(){
+        return ResponseEntity.ok(service.listBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BookDTO> updateBook(@PathVariable Long id, @RequestBody BookDTO dto){
+        return ResponseEntity.ok(service.updateBook(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Message> deleteBook(@PathVariable Long id){
+        return ResponseEntity.ok(
+                new Message(
+                        HttpStatus.NO_CONTENT.value(),
+                        "Livro com id" + id + " deletado."
+                )
+        );
+    }
+
 }
--- src/main/java/com/liaw/dev/Library/controller/UserController.java
+++ src/main/java/com/liaw/dev/Library/controller/UserController.java
@@ -1,11 +1,50 @@
 package com.liaw.dev.Library.controller;
 
+import com.liaw.dev.Library.dto.UserDTO;
+import com.liaw.dev.Library.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/library/user")
 public class UserController {
+
+    private final UserService service;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.createUser(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> listUser(){
+        return ResponseEntity.ok(service.listUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO dto){
+        return ResponseEntity.ok(service.updateUser(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Message> deleteUser(@PathVariable Long id){
+        return ResponseEntity.ok(
+                new Message(
+                        HttpStatus.NO_CONTENT.value(),
+                        "Usuário com id " + id + " deletado."
+                )
+        );
+    }
+
 }
