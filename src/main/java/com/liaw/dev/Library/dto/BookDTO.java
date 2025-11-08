797b71a5412abb4ffe09b777e1d762d3aea3e374
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-08 15:43:12 -0300

    feat: Create DTO of entity Book, User and Loan

--- src/main/java/com/liaw/dev/Library/dto/BookDTO.java
+++ src/main/java/com/liaw/dev/Library/dto/BookDTO.java
@@ -1,4 +1,21 @@
 package com.liaw.dev.Library.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.liaw.dev.Library.entity.Loan;
+import com.liaw.dev.Library.entity.User;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
 public class BookDTO {
+    private Long id;
+    private String name;
+    private String title;
+    private String author;
+    private String isbn;
+    private Boolean loan = false;
+    private User user;
+    private List<Loan> loans;
 }
--- src/main/java/com/liaw/dev/Library/dto/UserDTO.java
+++ src/main/java/com/liaw/dev/Library/dto/UserDTO.java
@@ -1,4 +1,18 @@
-package com.liaw.dev.Library.dto.BookDTO;
+package com.liaw.dev.Library.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.liaw.dev.Library.entity.Book;
+import com.liaw.dev.Library.entity.Loan;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
 
 public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private Integer registration;
+    List<Book> books;
+    private List<Loan> loans;
 }
