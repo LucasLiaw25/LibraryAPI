5d49159e2da2f9282114e24dc473c4dfaae73197
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-09 16:25:45 -0300

    feat: Crate method to find by isbn and registration

--- src/main/java/com/liaw/dev/Library/repository/BookRepository.java
+++ src/main/java/com/liaw/dev/Library/repository/BookRepository.java
@@ -3,5 +3,8 @@ package com.liaw.dev.Library.repository;
 import com.liaw.dev.Library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
 }
