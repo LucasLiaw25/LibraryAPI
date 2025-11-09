fd827ff057e992cb7ac5ad10fe24d077421a6110
LucasLiaw25 <lucasaliaw@gmail.com>
2025-11-09 16:24:45 -0300

    feat: Create method and endpoint to make a loan

--- src/main/java/com/liaw/dev/Library/controller/LoanController.java
+++ src/main/java/com/liaw/dev/Library/controller/LoanController.java
@@ -1,6 +1,13 @@
 package com.liaw.dev.Library.controller;
 
+import com.liaw.dev.Library.dto.LoanDTO;
+import com.liaw.dev.Library.dto.LoanRequest;
+import com.liaw.dev.Library.service.LoanService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/library/loan")
 public class LoanController {
+
+    private final LoanService service;
+
+    @PostMapping
+    public ResponseEntity<LoanDTO> makeLoan(@RequestBody LoanRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.makeLoan(request.registration(), request.isbn()));
+    }
+
 }
--- src/main/java/com/liaw/dev/Library/service/LoanService.java
+++ src/main/java/com/liaw/dev/Library/service/LoanService.java
@@ -1,14 +1,20 @@
 package com.liaw.dev.Library.service;
 
 import com.liaw.dev.Library.dto.LoanDTO;
+import com.liaw.dev.Library.entity.Book;
+import com.liaw.dev.Library.entity.Loan;
+import com.liaw.dev.Library.entity.User;
+import com.liaw.dev.Library.enums.LoanStatus;
 import com.liaw.dev.Library.mapper.LoanMapper;
 import com.liaw.dev.Library.mapper.UserMapper;
+import com.liaw.dev.Library.repository.BookRepository;
 import com.liaw.dev.Library.repository.LoanRepository;
 import com.liaw.dev.Library.repository.UserRepository;
 import com.liaw.dev.Library.validator.LoanValidator;
 import com.liaw.dev.Library.validator.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +23,43 @@ public class LoanService {
     private final LoanMapper mapper;
     private final LoanValidator validator;
     private final LoanRepository repository;
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
 
-    public void createLoan(){
+    @Transactional
+    public LoanDTO makeLoan(String registration, String isbn){
+        validator.validateLoan(registration, isbn);
+        User user = userRepository.findByRegistration(registration).get();
+        Book book = bookRepository.findByIsbn(isbn).get();
+
+        Loan loan = new Loan();
+        loan.setBook(book);
+        loan.setUser(user);
+        loan.setStatus(LoanStatus.PENDING);
+
+        repository.save(loan);
+
+        book.setLoan(true);
+        book.setUser(user);
+        book.getLoans().add(loan);
+        user.addBook(book);
+
+        userRepository.save(user);
+        bookRepository.save(book);
+
+
+        return mapper.toDTO(loan);
+
+    }
+
+    @Transactional
+    public void returnBook(String registration, String isbn){
+        validator.validateUserRequest(registration, isbn);
+
+        User user = userRepository.findByRegistration(registration).get();
+        Book book = bookRepository.findByIsbn(isbn).get();
+
+        book.setLoan(false);
 
     }
 
